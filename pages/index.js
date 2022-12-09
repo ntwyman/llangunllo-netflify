899e4752881d0025dc7ef52de01f0de2b84c2cfa
nick twyman <nick@conky.org>
2022-12-08 21:30:00 -0800

    Added local dev container, removed cypress (which I couldn't get to work in dev container) and added bare bones WWED?

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,28 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <div className="container" align="center">
+      <font face="Arial">
+        <a href="https://www.flickr.com/photos/92709190@N00/199589023" id="fs_1" title=""><img alt="W" border="0" src="https://static.flickr.com/70/199589023_134edfcea9_s.jpg" title="W" /></a>
+        <a href="https://www.flickr.com/photos/92709190@N00/243072078" id="fs_2" title="H"><img alt="H" border="0" src="https://static.flickr.com/97/243072078_622dae1237_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/183906037" id="fs_3" title="A"><img alt="A" border="0" src="https://static.flickr.com/45/183906037_dbac91e480_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/95229107@N00/109784905" id="fs_4" title="T"><img alt="T" border="0" src="https://static.flickr.com/38/109784905_59641b8f54_s.jpg" /></a>
+        <br />
+        <a href="https://www.flickr.com/photos/smallcurio/44573097831" id="fs_6" title=""><img alt="W" border="0" src="https://live.staticflickr.com/1849/44573097831_46a7bdb1a9_s.jpg" title="W" /></a>
+        <a href="https://www.flickr.com/photos/50318388@N00/177401729" id="fs_7" title="O"><img alt="O" border="0" src="https://static.flickr.com/51/177401729_01559125ab_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/95229107@N00/77106472" id="fs_8" title="U"><img alt="U" border="0" src="https://static.flickr.com/37/77106472_2368a2c897_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/26977713@N00/351774877" id="fs_9" title="L"><img alt="L" border="0" src="https://static.flickr.com/160/351774877_30188328f5_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/105785245" id="fs_10" title="D"><img alt="D" border="0" src="https://static.flickr.com/44/105785245_cc8c5da30b_s.jpg" /></a>
+        <br />
+        <a href="https://www.flickr.com/photos/92709190@N00/190167062" id="fs_12" title=""><img alt="Ee" border="0" src="https://static.flickr.com/60/190167062_4193527381_s.jpg" title="E" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/159165753" id="fs_13" title="R"><img alt="R" border="0" src="https://static.flickr.com/46/159165753_5bd919d960_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/158263738" id="fs_14" title="I"><img alt="I" border="0" src="https://static.flickr.com/69/158263738_560b10f03a_s.jpg" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/158768216" id="fs_15" title="C"><img alt="C" border="0" src="https://static.flickr.com/61/158768216_e1f56d4356_s.jpg" /></a>
+        <br />
+        <a href="https://www.flickr.com/photos/49968232@N00/159166847" id="fs_17" title=""><img alt="D" border="0" src="https://static.flickr.com/63/159166847_12859886e6_s.jpg" title="D" /></a>
+        <a href="https://www.flickr.com/photos/49968232@N00/158885571" id="fs_18" title=""><img alt="O" border="0" src="https://static.flickr.com/75/158885571_bd60ec5121_s.jpg" title="O" /></a>
+        <a href="https://www.flickr.com/photos/76207980@N00/32879751" id="fs_19" title=""><img alt="?" border="0" src="https://static.flickr.com/23/32879751_a5974b3149_s.jpg" title="?" /></a>
+      </font>
+    </div >
   )
 }
